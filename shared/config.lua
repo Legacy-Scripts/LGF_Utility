e6df31734d0c75add363234b1450b428e3e16703
ENT510 <145626625+ENT510@users.noreply.github.com>
2024-10-07 01:11:14 +0200

    Update config.lua

--- shared/config.lua
+++ shared/config.lua
@@ -5,6 +5,6 @@ Config.isPlayerDead = function()
     if GetResourceState('ars_ambulancejob'):find('start') then
         return LocalPlayer.state.dead
     else
-        return IsEntityDead(cache.ped)
+        return IsEntityDead(PlayerPedId())
     end
 end
